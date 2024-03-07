5f188e4f2dc502587cbf6c847b9d2dc2916513f1
Maryna Zubkova <maryna.zubkova@lanars.com>
2024-03-07 17:09:03 +0200

    Update scale values for cursor

--- src/components/MagneticFramer.tsx
+++ src/components/MagneticFramer.tsx
@@ -7,36 +7,70 @@ const MagneticFramer = ({ children }: { children: ReactNode }) => {
   const cursor = document.getElementById('cursor-dot');
 
   const handleMouse = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const { clientX, clientY } = e;
+    const { clientX, clientY, pageX, pageY } = e;
     const { height, width, left, top } = ref.current!.getBoundingClientRect();
 
     //horizontal center point (X-axis) and vertical center point (Y-axis)
     const centerX = left + width / 2;
     const centerY = top + height / 2;
 
-    //the distance between the cursor position (clientX, clientY) and the center of the element
-    const offsetX = clientX - centerX;
-    const offsetY = clientY - centerY;
+    /*
+    the distance between the cursor position (clientX, clientY)
+    and the center of the element (relative to the browser window)
+    */
+    const offsetClientX = clientX - centerX;
+    const offsetClientY = clientY - centerY;
 
-    //normalized values of the mouse cursor deviation from the center of the element
-    const normalizedX = offsetX / (width / 2);
-    const normalizedY = offsetY / (height / 2);
+    /*
+    the distance between the cursor position (clientX, clientY)
+    and the center of the element (relative to the whole document)
+    */
+    const offsetDocumentX = pageX - ref.current!.offsetLeft;
+    const offsetDocumentY = pageY - ref.current!.offsetTop;
 
-    //these values are scaled by 5 to increase the effect and define new cursor positions
+    /*
+    normalized values of the mouse cursor deviation
+    to the center of the element
+    */
+    const normalizedX = offsetClientX / (width / 2);
+    const normalizedY = offsetClientY / (height / 2);
+
+    /*
+    normalized values of the mouse cursor deviation
+    to the top left corner of the page.
+    */
+    const normalizedDocumentX = offsetDocumentX / (width / 2);
+    const normalizedDocumentY = offsetDocumentY / (height / 2);
+
+    /*
+    these values are scaled by 5 to increase the effect
+    and define new cursor positions
+    */
     const middleX = normalizedX * 5;
     const middleY = normalizedY * 5;
 
     setPosition({ x: middleX, y: middleY });
 
     //calculate rotation angle based on cursor position
-    const angle = Math.atan2(centerY, centerX) * (180 / Math.PI);
+    const angle = -Math.atan2(offsetClientX, offsetClientY);
 
     if (cursor) {
       cursor.style.willChange = 'transform';
-      cursor.style.transform = `translate(${middleX}px, ${middleY}px) rotate(${angle}deg)`;
+      cursor.style.transform = `translate(${middleX}px, ${middleY}px) rotate(${angle}rad)`;
       const scaleWidth = 1 - Math.abs(normalizedX) * 0.1;
       const scaleHeight = 1 - Math.abs(normalizedY) * 0.1;
-      cursor.style.transform += `scale(${scaleWidth}, ${scaleHeight})`;
+      const scaleWidthDocument = 1 - Math.abs(normalizedDocumentX) * 0.1;
+      const scaleHeightDocument = 1 - Math.abs(normalizedDocumentY) * 0.1;
+
+      cursor.style.transform += `scale(${
+        scaleWidth < scaleHeight
+          ? Math.max(scaleWidth, scaleWidthDocument)
+          : Math.max(scaleHeight, scaleHeightDocument)
+      }, ${
+        scaleWidth < scaleHeight
+          ? Math.max(scaleHeight, scaleHeightDocument)
+          : Math.max(scaleWidth, scaleWidthDocument)
+      })`;
     }
   };
 
